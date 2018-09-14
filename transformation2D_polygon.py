3bd32c36e5dd425f56fce19f2c772805d80b08c6
Manish Kumar Chaudhary <42311706+chaudharymanishkumar@users.noreply.github.com>
2018-09-14 22:34:08 +0530

    Update transformation2D_polygon.py

--- transformation2D_polygon.py
+++ transformation2D_polygon.py
@@ -12,7 +12,7 @@ import sys
 from math import * 
 matrix=[[0 for x in range(1)] for y in range(3)]               # 3*1 matrix to store homogenious point (x,y)
 composite_matrix=[[0 for x in range(3)] for y in range(3)]     # 3*3 composite matrix
-def init():
+def init():						`     
 	glClearColor(0.0,1.0,1.0,0.0)
 	glColor3f(1.0,0.0,0.0)
 	glPointSize(2.0)
