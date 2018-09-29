9c8ad67faa0a8661ff9fecdcc3023ea05d106fcb
Manish Kumar Chaudhary <42311706+chaudharymanishkumar@users.noreply.github.com>
2018-09-29 18:00:52 +0530

    Add files via upload

---
+++ Bezier_curve.py
@@ -0,0 +1,81 @@
+#Mkchaudhary 17sept 2018
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from math import *
+from time import *
+import sys
+
+def init():
+	glClearColor(0.0,1.0,1.0,0.0)
+	glColor3f(1.0,0.0,0.0)
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
+	glPointSize(2.0)
+	gluOrtho2D(0,599,0,599)
+		
+def setPixel(xcoordinate,ycoordinate):
+	glBegin(GL_POINTS)
+	glVertex2f(xcoordinate,ycoordinate)
+	glEnd()
+	glFlush()
+
+def read_controlpoint():
+	global px,py
+	n=input("Enter no of control points: ")
+	px=[0 for x in range(n)]
+	py=[0 for y in range(n)]
+	for i in range(n):
+		px[i]=input("Enter control point_x: ")
+		py[i]=input("Enter control point_y: ")
+
+def factorial(n):
+	if n==0:
+		return 1
+	else:
+		n=n*factorial(n-1) 
+	return n
+
+def Binomial_coefficient(n,k):
+	result=factorial(n)/(factorial(n-k)*factorial(k))
+	return result
+
+def Bezier():
+	n=len(px)-1
+	u=0.0
+	while u<=1.0:	
+		x=0.0
+		y=0.0
+		k=0
+		for k in range(n+1):
+			x+=Binomial_coefficient(n,k)*pow(u,k)*pow(1-u,n-k)*px[k]
+			y+=Binomial_coefficient(n,k)*pow(u,k)*pow(1-u,n-k)*py[k]
+		setPixel(x,y)		
+		u+=0.0001
+		
+def draw_Bezier_curve():
+	while True:
+		read_controlpoint()
+		Bezier()
+		print("Enter any decimal to continue")
+		check=int(input("Enter 0 to exit: "))
+		if check==0:
+			sleep(5)
+			sys.exit()
+		else:
+			pass
+def Display():
+	glClear(GL_COLOR_BUFFER_BIT)
+	draw_Bezier_curve()
+
+def main():
+	glutInit(sys.argv)
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+	glutInitWindowSize(600,600)
+	glutInitWindowPosition(50,50)
+	glutCreateWindow("Bezier curve")
+	glutDisplayFunc(Display)
+	init()
+	glutMainLoop()
+
+main()
